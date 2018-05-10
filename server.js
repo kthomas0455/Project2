f632229a4940bbb3ffc3fbaecef8eae70ebc9e53
Ryan <rjlafazan@gmail.com>
2018-05-10 12:40:16 -0700

    general server setup

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
+app.use(express.static("public"));
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
