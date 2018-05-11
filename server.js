99355426cb81787d88cb3e1febeade5e3dcf04a0
Ryan <rjlafazan@gmail.com>
2018-05-11 09:37:52 -0700

    beginning of twilio setup

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
-var app = express();
+const app = express();
 var PORT = process.env.PORT || 8080;
 
 var db = require("./models");
@@ -19,4 +21,19 @@ db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+});
+
+//TWILIO server details
+
+app.post("/sms", (req, res) => {
+  const twiml = new MessagingResponse();
+
+  twiml.message("The Robots are coming! Head for the hills!");
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(PORT, () => {
+  console.log("Express server listening on port: " + PORT);
 });
