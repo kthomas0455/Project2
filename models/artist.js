c5d62856bab7f022b5ff226da20e6d8e4ee66bd7
SiriusScaper <siriusscaper@gmail.com>
2018-05-15 00:10:54 -0700

    Working on search by zipcode and  corrected some route inconsistencies artists vs artist. Added some frontend jquery functions to capture user input in zipcode search

--- models/artist.js
+++ models/artist.js
@@ -1,94 +1,96 @@
 module.exports = function(sequelize, DataTypes) {
-  //Basic posting to database. More can be added at any time
-  var Artist = sequelize.define("artists", {
-    artistName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-      }
-    },
-    location: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-      }
-    },
-    artistNumber: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-      }
-    },
-    independent: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-      }
-    },
-    shopName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-      }
-    },
-    hourlyRate: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-      }
-    },
-    artistMin: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-      }
-    },
-    stylePref: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-      }
-    },
-    artistInsta: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-      }
-    },
-    shopLink: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-      }
-    },
-    artistBio: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        len: [1],
-      },
-    },
-    artistComments: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        len: [1],
-      }
-    }
-  },
- {
-    timestamps: false
-}
-  );
-  return Artist;
+	//Basic posting to database. More can be added at any time
+	var Artist = sequelize.define(
+		'artists',
+		{
+			artistName: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					len: [1]
+				}
+			},
+			location: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					len: [1]
+				}
+			},
+			artistNumber: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					len: [1]
+				}
+			},
+			independent: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					len: [1]
+				}
+			},
+			shopName: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					len: [1]
+				}
+			},
+			hourlyRate: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					len: [1]
+				}
+			},
+			artistMin: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					len: [1]
+				}
+			},
+			stylePref: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					len: [1]
+				}
+			},
+			artistInsta: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					len: [1]
+				}
+			},
+			shopLink: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					len: [1]
+				}
+			},
+			artistBio: {
+				type: DataTypes.TEXT,
+				allowNull: false,
+				validate: {
+					len: [1]
+				}
+			},
+			artistComments: {
+				type: DataTypes.TEXT,
+				allowNull: false,
+				validate: {
+					len: [1]
+				}
+			}
+		},
+		{
+			timestamps: false
+		}
+	);
+	return Artist;
 };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,184 +4,180 @@
 
 // Dependencies
 // =============================================================
-var db = require("../models");
+var db = require('../models');
 
-var path = require("path");
+var path = require('path');
 
 // Routes
 // =============================================================
 module.exports = function(app) {
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+	// Each of the below routes just handles the HTML page that the user gets sent to.
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
+	app.get('/', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
+	});
 
-  app.get("/register", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/register.html"));
-  });
+	app.get('/register', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/register.html'));
+	});
 
- app.get("/contact", function(req, res) {
-    res.render("contact");
-  });
+	app.get('/contact', function(req, res) {
+		res.render('contact');
+	});
 
-  app.get("/search/:id", function(req, res) {
+	app.get('/search/:id', function(req, res) {
+		db.artists
+			.findAll({
+				where: {
+					id: req.params.id
+				}
+			})
+			.then(function(dbArtists) {
+				res.render('profile', { artist: dbArtists[0] });
+			});
+	});
 
-    db.artists.findAll({
-      where: {
-        id: req.params.id,
-      },
-    }).then(function(dbArtists) {
-      res.render("profile", {artist: dbArtists[0]});
-    });
-    
-  });
+	// Search route to handle searching by zipcode
+	app.get('/search/zipcode/:zipcode', (req, res) => {
+		// search artists whose address have teh zip code to search by
+		db.artists
+			.findAll({
+				where: { location: { $like: `${req.params.zipcode}` } }sequelize.where(sequelize.fn('LOWER', sequelize.col('asset_name')), 'LIKE', '%' + lookupValue + '%')
+			})
+			.then(dbArtists => {
+				res.render('index', { artist: dbArtists });
+			});
+	});
 
-  app.get("/search", function(req, res) {
+	app.get('/search', function(req, res) {
+		// geocoder section here
 
-    // geocoder section here 
+		// get stuff from artists table
+		db.artists.findAll({}).then(function(dbArtists) {
+			res.render('index', { artists: dbArtists });
+		});
+		// var artists = []
+	});
 
-    // get stuff from artists table
-    db.artists.findAll({}).then(function(dbArtists) {
+	app.get('/lowest-to-highest', function(req, res) {
+		db.artists
+			.findAll({
+				order: [
+					db.sequelize.fn('isnull', db.sequelize.col('hourlyRate')),
+					['hourlyRate', 'ASC']
+				]
+			})
+			.then(function(dbArtists) {
+				res.render('lowest', { artists: dbArtists });
+			});
+	});
 
-      res.render("index", {artists: dbArtists});
-    });
-    // var artists = []
+	app.get('/highest-to-lowest', function(req, res) {
+		db.artists
+			.findAll({
+				order: [
+					db.sequelize.fn('isnull', db.sequelize.col('hourlyRate')),
+					['hourlyRate', 'DESC']
+				]
+			})
+			.then(function(dbArtists) {
+				res.render('highest', { artists: dbArtists });
+			});
+	});
 
-    
-  });
+	app.get('/traditional', function(req, res) {
+		db.artists
+			.findAll({
+				where: {
+					stylePref: 'Traditional'
+				}
+			})
+			.then(function(dbArtists) {
+				res.render('traditional', { artists: dbArtists });
+			});
+	});
 
-  app.get("/lowest-to-highest", function(req, res) {
+	app.get('/tribal', function(req, res) {
+		db.artists
+			.findAll({
+				where: {
+					stylePref: 'Tribal'
+				}
+			})
+			.then(function(dbArtists) {
+				res.render('tribal', { artists: dbArtists });
+			});
+	});
 
-    db.artists.findAll({
-        order: [
-          db.sequelize.fn("isnull", db.sequelize.col("hourlyRate")),
-          ["hourlyRate", "ASC"],
-        ],
-      })
-      .then(function(dbArtists) {
-        res.render("lowest", {artists: dbArtists});
-      });
+	app.get('/japanese', function(req, res) {
+		db.artist
+			.findAll({
+				where: {
+					stylePref: 'Japanese'
+				}
+			})
+			.then(function(dbArtists) {
+				res.render('japanese', { artists: dbArtists });
+			});
+	});
 
-  });
+	app.get('/blackwork', function(req, res) {
+		db.artists
+			.findAll({
+				where: {
+					stylePref: 'Blackwork'
+				}
+			})
+			.then(function(dbArtists) {
+				res.render('blackwork', { artists: dbArtists });
+			});
+	});
 
-  app.get("/highest-to-lowest", function(req, res) {
+	app.get('/minimalist', function(req, res) {
+		db.artists
+			.findAll({
+				where: {
+					stylePref: 'Minimalist'
+				}
+			})
+			.then(function(dbArtists) {
+				res.render('minimalist', { artists: dbArtists });
+			});
+	});
 
-    db.artists.findAll({
-        order: [
-          db.sequelize.fn("isnull", db.sequelize.col("hourlyRate")),
-          ["hourlyRate", "DESC"],
-        ],
-      })
-      .then(function(dbArtists) {
-        res.render("highest", {artists: dbArtists});
-      });
+	app.get('/new-school', function(req, res) {
+		db.artists
+			.findAll({
+				where: {
+					stylePref: 'New School'
+				}
+			})
+			.then(function(dbArtists) {
+				res.render('new-school', { artists: dbArtists });
+			});
+	});
 
-  });
+	app.get('/realist', function(req, res) {
+		db.artists
+			.findAll({
+				where: {
+					stylePref: 'Realism'
+				}
+			})
+			.then(function(dbArtists) {
+				res.render('realist', { artists: dbArtists });
+			});
+	});
 
-
-app.get("/traditional", function(req, res) {
-
-    db.artists.findAll({
-        where: {
-          stylePref: "Traditional",
-        },
-      })
-      .then(function(dbArtists) {
-        res.render("traditional", {artists: dbArtists});
-      });
-
-  });
-
-app.get("/tribal", function(req, res) {
-
-    db.artists.findAll({
-        where: {
-          stylePref: "Tribal",
-        },
-      })
-      .then(function(dbArtists) {
-        res.render("tribal", {artists: dbArtists});
-      });
-
-  });
-
-app.get("/japanese", function(req, res) {
-
-    db.artists.findAll({
-        where: {
-          stylePref: "Japanese",
-        },
-      })
-      .then(function(dbArtists) {
-        res.render("japanese", {artists: dbArtists});
-      });
-
-  });
-
-app.get("/blackwork", function(req, res) {
-
-    db.artists.findAll({
-        where: {
-          stylePref: "Blackwork",
-        },
-      })
-      .then(function(dbArtists) {
-        res.render("blackwork", {artists: dbArtists});
-      });
-
-  });
-
-app.get("/minimalist", function(req, res) {
-
-    db.artists.findAll({
-        where: {
-          stylePref: "Minimalist",
-        },
-      })
-      .then(function(dbArtists) {
-        res.render("minimalist", {artists: dbArtists});
-      });
-
-  });
-
-app.get("/new-school", function(req, res) {
-
-    db.artists.findAll({
-        where: {
-          stylePref: "New School",
-        },
-      })
-      .then(function(dbArtists) {
-        res.render("new-school", {artists: dbArtists});
-      });
-
-  });
-
-app.get("/realist", function(req, res) {
-
-    db.artists.findAll({
-        where: {
-          stylePref: "Realism",
-        },
-      })
-      .then(function(dbArtists) {
-        res.render("realist", {artists: dbArtists});
-      });
-
-  });
-
-app.get("/watercolor", function(req, res) {
-
-    db.artists.findAll({
-        where: {
-          stylePref: "Watercolor",
-        },
-      })
-      .then(function(dbArtists) {
-        res.render("watercolor", {artists: dbArtists});
-      });
-
-  });
-
+	app.get('/watercolor', function(req, res) {
+		db.artists
+			.findAll({
+				where: {
+					stylePref: 'Watercolor'
+				}
+			})
+			.then(function(dbArtists) {
+				res.render('watercolor', { artists: dbArtists });
+			});
+	});
 };
